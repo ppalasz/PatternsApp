1a4038be7b0103f47ac874395a97b356b0f73148
Paweł Pałasz <ppalasz@poczta.onet.pl>
2018-07-28 22:28:30 +0200

    1st commit

---
+++ PatternsApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PatternsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            using (TestClassB t = new TestClassB("hej"))
+            {
+
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ PatternsApp/TestClass.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PatternsApp
+{
+    public class TestClass:IDisposable
+    {
+        static TestClass()
+        {
+            Console.WriteLine("static constructor");
+        }
+
+        public TestClass()
+        {
+            Console.WriteLine("constructor");
+        }
+
+        public TestClass(string name)
+        {
+            Console.WriteLine("constructor "+ name);
+        }
+
+        ~TestClass()
+        {
+            Console.WriteLine("destructor");
+        }
+
+        public virtual void Dispose()
+        {
+            Console.WriteLine("dispose");
+        }
+    }
+
+    public class TestClassB : TestClass
+    {
+        static TestClassB()
+        {
+            Console.WriteLine("static constructor B");
+        }
+
+        public TestClassB(string name) : base(name)
+        {
+            Console.WriteLine("constructor B " + name);
+        }
+
+        public override void Dispose()
+        {
+            //base.Dispose();
+            Console.WriteLine("dispose B");
+            
+        }
+
+        
+    }
+}
